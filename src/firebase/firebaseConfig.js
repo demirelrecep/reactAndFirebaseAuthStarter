b7a5d84032d4297357b14756045ffaab24008386
Recep Demirel <35351559+demirelrecep@users.noreply.github.com>
2021-04-07 13:49:08 +0300

    Deleted Firebase config

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,14 +3,14 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC6f3Yxh6351u-3tx4RuTMycFkLZHgRDyw",
-    authDomain: "rgaproject-38a15.firebaseapp.com",
-    databaseURL: "https://rgaproject-38a15.firebaseio.com",
-    projectId: "rgaproject-38a15",
-    storageBucket: "rgaproject-38a15.appspot.com",
-    messagingSenderId: "581027606258",
-    appId: "1:581027606258:web:84c2d3087c7ca5e7b8aa6b",
-    measurementId: "G-F3CQ5WTRYZ"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = firebase.initializeApp(firebaseConfig);
